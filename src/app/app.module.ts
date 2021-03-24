8053d7a6de7c3c613160c8eb858f81af7fe4a134
Anuj-Kushwah-ak <kushwahanuj.ak@gmail.com>
2021-03-24 16:48:45 +0530

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { CoursesComponent } from './courses/courses.component';
-import { CourseDetailComponent } from './course-detail/course-detail.component';
-import { HomeComponent } from './home/home.component';
-
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CoursesComponent } from "./courses/courses.component";
+import { CourseDetailComponent } from "./course-detail/course-detail.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CoursesComponent, CourseDetailComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ],
- 
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CoursesComponent,
+    CourseDetailComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
