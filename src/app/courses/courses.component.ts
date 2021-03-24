e3cc12c8b375891fb8d6f16622953ac535b30e37
Anuj-Kushwah-ak <kushwahanuj.ak@gmail.com>
2021-03-24 13:56:57 +0530

    2 files modified, 1 added

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "../course";
+import { COURSES } from "../mock";
 @Component({
   selector: "app-courses",
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.css"]
 })
 export class CoursesComponent implements OnInit {
-  course: Course = {
-    id: 1,
-    name: "angular"
-  };
+  courses = COURSES ;
   constructor() {}
 
   ngOnInit() {}
