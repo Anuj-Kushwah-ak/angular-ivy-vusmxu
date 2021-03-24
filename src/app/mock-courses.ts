ac7667768546c67ea10559fe7552c0cd5216d4e2
Anuj-Kushwah-ak <kushwahanuj.ak@gmail.com>
2021-03-24 16:55:02 +0530

    1 file modified

--- src/app/mock-courses.ts
+++ src/app/mock-courses.ts
@@ -2,10 +2,10 @@ import { Course } from "./course";
 
 export const COURSES: Course[] = [
   { id: 1, name: " angA" },
-  { id: 1, name: " angB" },
-  { id: 1, name: " angC" },
-  { id: 1, name: " angD" },
-  { id: 1, name: " angE" },
-  { id: 1, name: " angF" },
-  { id: 1, name: " angG" }
+  { id: 2, name: " angB" },
+  { id: 3, name: " angC" },
+  { id: 4, name: " angD" },
+  { id: 5, name: " angE" },
+  { id: 6, name: " angF" },
+  { id: 7, name: " angG" }
 ];
